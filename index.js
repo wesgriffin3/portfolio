4ba59f98f07d301cbbb2831b41af7a8335e9cd9c
Wes Griffin <WesGriffin@Wess-MacBook-Pro.local>
2020-11-16 13:35:07 -0600

    updated finishing touches

---
+++ index.js
@@ -0,0 +1,45 @@
+// mobile navbar
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  // toggle nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    //   animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.75
+        }s`;
+      }
+    });
+    // burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
+
+// sticky navbar
+
+// when scrolling, execute
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("navbar");
+
+// get offset position of the nav
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
